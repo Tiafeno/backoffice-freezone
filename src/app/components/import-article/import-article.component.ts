b7a69c5c36f1db94fdf11ef89183a22d7b1df93f
Tiafeno <tiafenofnel@gmail.com>
2019-06-12 22:32:27 +0300

    Correction pour la modification d'article

--- src/app/components/import-article/import-article.component.ts
+++ src/app/components/import-article/import-article.component.ts
@@ -10,217 +10,242 @@ import 'rxjs/add/observable/zip';
 import { Helpers } from '../../helpers';
 import { forEach } from '@angular/router/src/utils/collection';
 import { ApiWoocommerceService } from '../../_services/api-woocommerce.service';
+import { reject } from 'q';
 
 @Component({
-  selector: 'app-import-article',
-  templateUrl: './import-article.component.html',
-  styleUrls: ['./import-article.component.css']
+    selector: 'app-import-article',
+    templateUrl: './import-article.component.html',
+    styleUrls: ['./import-article.component.css']
 })
 export class ImportArticleComponent implements OnInit {
-  public Form: FormGroup;
-  public Files: any;
-  private Columns: any = {
-    Title: null,
-    ProductRef: null,
-    SupplierRef: null,
-    Qty: 1,
-    Price: 0,
-    LastUpdate: null, // Date
-    Categorie: '',
-    Mark: '',
-    Marge: ''
-  };
+    public Form: FormGroup;
+    public Files: any;
+    private Columns: any = {
+        Title: null,
+        SupplierRef: null,
+        Qty: 1,
+        Price: 0,
+        PriceDealer: 0,
+        LastUpdate: null, // Date
+        Categorie: '',
+        Mark: '',
+        Marge: '',
+        MargeDealer: '',
+    };
 
-  public loopColumn: Array<any> = [
-    { key: 'Title', label: "Titre" },
-    { key: 'ProductRef', label: "Réference du produit Freezone" },
-    { key: 'SupplierRef', label: "Réference du fournisseur" },
-    { key: 'Price', label: "Prix du fournisseur" },
-    { key: 'Qty', label: "Quantité" },
-    { key: 'LastUpdate', label: "Date de revision" },
-    { key: 'Categorie', label: "Categories" },
-    { key: 'Mark', label: "Marque" },
-    { key: 'Marge', label: "Marge" },
-  ]
+    public loopColumn: Array<any> = [
+        { key: 'Title', label: "Titre" },
+        { key: 'SupplierRef', label: "Réference du fournisseur" },
+        { key: 'Price', label: "Prix fournisseur" },
+        { key: 'PriceDealer', label: "Prix revendeur" },
+        { key: 'Qty', label: "Quantité" },
+        { key: 'LastUpdate', label: "Date de revision" },
+        { key: 'Categorie', label: "Categories" },
+        { key: 'Mark', label: "Marque" },
+        { key: 'Marge', label: "Marge" },
+        { key: 'MargeDealder', label: "Marge de revendeur" },
+    ]
 
-  public Inputs: Array<any>;
-  private Wordpress: any;
-  private Woocommerce: any;
+    public Inputs: Array<any>;
+    private Wordpress: any;
+    private Woocommerce: any;
 
-  @Output() refresh = new EventEmitter();
-  constructor(
-    private apiWP: ApiWordpressService,
-    private apiWC: ApiWoocommerceService,
-    private cd: ChangeDetectorRef
-  ) {
-    this.Form = new FormGroup({ csv: new FormControl({ value: "" }, Validators.required) });
-    this.Wordpress = this.apiWP.getWPAPI();
-    this.Woocommerce = this.apiWC.getWoocommerce();
-  }
-
-  ngOnInit() {
-  }
-
-  onFileChange($event): void | boolean {
-    this.Files = $event.target.files[0];
-    this.Form.controls['csv'].setValue(this.Files ? this.Files.name : ''); // <-- Set Value for Validation
-
-    if (_.isUndefined(this.Files)) {
-      this.Inputs = [];
-      return false;
+    @Output() refresh = new EventEmitter();
+    constructor(
+        private apiWP: ApiWordpressService,
+        private apiWC: ApiWoocommerceService,
+        private cd: ChangeDetectorRef
+    ) {
+        this.Form = new FormGroup({ csv: new FormControl({ value: "" }, Validators.required) });
+        this.Wordpress = this.apiWP.getWPAPI();
+        this.Woocommerce = this.apiWC.getWoocommerce();
     }
-    Papa.parse(this.Files, {
-      complete: (results) => {
-        if (results.data.length > 0) {
-          this.Inputs = results.data[0];
-          this.cd.detectChanges();
+
+    ngOnInit() {
+    }
+
+    onFileChange($event): void | boolean {
+        this.Files = $event.target.files[0];
+        this.Form.controls['csv'].setValue(this.Files ? this.Files.name : ''); // <-- Set Value for Validation
+
+        if (_.isUndefined(this.Files)) {
+            this.Inputs = [];
+            return false;
         }
-      }
-    });
-  }
+        Papa.parse(this.Files, {
+            complete: (results) => {
+                if (results.data.length > 0) {
+                    this.Inputs = results.data[0];
+                    this.cd.detectChanges();
+                }
+            }
+        });
+    }
 
-  onSelectColumn($event, index) {
-    let element: any = $event.currentTarget;
-    let key: string = $(element).val();
+    onSelectColumn($event, index) {
+        let element: any = $event.currentTarget;
+        let key: string = $(element).val();
 
-    this.Columns[key] = index;
+        this.Columns[key] = index;
 
-    console.log(this.Columns);
-  }
+        console.log(this.Columns);
+    }
 
-  onSubmit(): void | boolean {
-    if (this.Form.invalid) return false;
-    Helpers.setLoading(true);
-    Papa.parse(this.Files, {
-      delimiter: ';',
-      preview: 0,
-      step: async (results: any, parser: any) => {
-        if (results || results.data) {
-          parser.pause();
-          const column: any = results.data;
-          if (_.isNull(this.Columns.ProductRef)) parser.resume();
+    onSubmit(): void | boolean {
+        if (this.Form.invalid) return false;
+        Helpers.setLoading(true);
+        Papa.parse(this.Files, {
+            delimiter: ';',
+            preview: 0,
+            step: async (results: any, parser: any) => {
+                parser.pause();
+                if (results || results.data) {
+                    const column: any = results.data;
+                    const price: number = parseInt(column[this.Columns.Price]);
+                    if (_.isNaN(price)) {
+                        console.log('is NaN!');
+                        parser.resume();
+                        return;
+                    }
 
-          //let productsRef = this.Wordpress.products().sku(column[this.Columns.ProductRef]);
-          let categorie: string = column[this.Columns.Categorie];
-          let _categories: Array<any> = await this.createCategories(_.split(categorie, ','));
-          _categories = _.map(_categories, (ctg) => { return { id: ctg.id } });
-          let args: object = {
-            type: 'simple',
-            status: 'publish',
-            name: column[this.Columns.Title],
-            regular_price: '0',
-            description: '',
-            short_description: '',
-            categories: _categories,
-            attributes: [
-              {
-                name: 'brands',
-                options: column[this.Columns.Mark],
-                visible: true
-              }
-            ],
-            meta_data: [
-              {key: '_fz_marge', value: column[this.Columns.Marge]}
-            ],
-            images: []
-          };
-          let productRef = this.createProduct(args); // Crée le produit
-          let supplier = this.Wordpress.users().roles('fz-supplier').filter({ meta_key: 'reference', meta_value: column[this.Columns.SupplierRef] });
-          Observable.zip(productRef, supplier).subscribe(dataObs => {
+                    let categorie: string = column[this.Columns.Categorie];
+                    let _categories: Array<any> = await this.createCategories(_.split(categorie, ','));
+                    _categories = _.map(_categories, (ctg) => { return { id: ctg.id } });
+                    let args: object = {
+                        type: 'simple',
+                        status: 'publish',
+                        name: column[this.Columns.Title],
+                        regular_price: '0',
+                        description: '',
+                        short_description: '',
+                        categories: _categories,
+                        attributes: [
+                            {
+                                name: 'brands',
+                                options: column[this.Columns.Mark],
+                                visible: true
+                            }
+                        ],
+                        meta_data: [
+                            { key: '_fz_marge', value: column[this.Columns.Marge] },
+                            { key: '_fz_marge_dealer', value: column[this.Columns.MargeDealer] }
+                        ],
+                        images: []
+                    };
+                    let productRef = this.createProduct(args); // Crée le produit
+                    let supplier = this.Wordpress.users().roles('fz-supplier').filter({ meta_key: 'reference', meta_value: column[this.Columns.SupplierRef] });
+                    Observable.zip(productRef, supplier).subscribe(dataObs => {
 
-            // return value with pagination (property: _paging)
-            let product: any = dataObs[0][0];
-            let supplier: any = dataObs[1][0];
+                        // return value with pagination (property: _paging)
+                        let product: any = dataObs[0];
+                        let supplier: any = dataObs[1][0];
 
-            if (_.isEmpty(product) || _.isEmpty(supplier)) {
-              parser.resume();
-              return;
+                        if (_.isEmpty(product) || _.isEmpty(supplier)) {
+                            parser.resume();
+                            return;
+                        }
+
+                        this.Wordpress
+                            .fz_product()
+                            .create({
+                                title: _.isNull(this.Columns.Title) ? product.name : column[this.Columns.Title],
+                                content: product.description,
+                                status: 'publish',
+                                price: column[this.Columns.Price],
+                                price_dealer: column[this.Columns.PriceDealer],
+                                date_add: moment().format('YYYY-MM-DD HH:mm:ss'),
+                                date_review: moment().format('YYYY-MM-DD HH:mm:ss'),
+                                product_id: product.id,
+                                total_sales: this.Columns.Qty.toString(),
+                                user_id: supplier.id,
+                                product_cat: _.map(product.categories, (ctg) => ctg.id)
+                            }).then(() => {
+                                parser.resume();
+                            }).catch(function (err) {
+                                parser.abord();
+                            })
+
+                    });
+                } else {
+                    parser.resume();
+                }
+            },
+            encoding: "UTF-8",
+            complete: (results: any) => {
+                console.log("Finished:", results.data);
+                $('#import-article-modal').modal('hide');
+                Helpers.setLoading(false);
+                this.refresh.emit();
+            },
+            error: this.errorFn,
+            dynamicTyping: true,
+            //header: true,
+            download: false,
+            before: (file, inputElement) => {
+
+            }
+        });
+    }
+
+    createProduct(args: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+
+            this.Woocommerce.post("products", args, (err, data, response) => {
+                let product: any = JSON.parse(response);
+                this.Woocommerce.put(`products/${product.id}`, { sku: `PRD${product.id}` }, (err, data, res) => {
+                    let response: any = JSON.parse(res);
+                    if (_.isUndefined(response.code)) {
+                        resolve(response);
+                    } else {
+                        resolve({id: response.data.resource_id, name: '', description: ''});
+                    }
+                });
+            });
+        })
+    }
+
+    private createCategories(ctgs: any): Promise<Array<any>> {
+        let results: Array<any> = [];
+        return new Promise(async (resolve, reject) => {
+            let categories: Array<string> = _.map(ctgs, ctg => _.trim(ctg));
+            categories = _.filter(categories, ctg => !_.isEmpty(ctg));
+            if (_.isEmpty(categories)) resolve([]);
+
+            for (let ctg of categories) {
+                let categorie: any = await this.createProductCategory(ctg);
+                if (!categorie) continue;
+                results.push(categorie);
             }
 
-            this.Wordpress
-              .fz_product()
-              .create({
-                title: _.isNull(this.Columns.Title) ? product.name : column[this.Columns.Title],
-                content: product.description,
-                status: 'publish',
-                price: column[this.Columns.Price],
-                date_add: moment().format('YYYY-MM-DD HH:mm:ss'),
-                date_review: moment().format('YYYY-MM-DD HH:mm:ss'),
-                product_id: product.id,
-                total_sales: this.Columns.Qty.toString(),
-                user_id: supplier.id
-              }).then(article => {
-                parser.resume();
-              }).catch(function (err) {
-                parser.abord();
-              })
+            resolve(results);
 
-          });
-        }
-      },
-      encoding: "UTF-8",
-      complete: (results: any) => {
-        console.log("Finished:", results.data);
-        $('#import-article-modal').modal('hide');
-        Helpers.setLoading(false);
-        this.refresh.emit();
-      },
-      error: this.errorFn,
-      dynamicTyping: true,
-      //header: true,
-      download: false,
-      before: (file, inputElement) => {
-
-      }
-    })
-  }
-
-  createProduct(args: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.Woocommerce.post("products", args, (err, data, response) => {
-        let product: any = JSON.parse(response);
-        this.Woocommerce.put(`products/${product.id}`, {sku: `PRD${product.id}`}, (err, data, res) => {
-            resolve(JSON.parse(res))
         });
-        //resolve(product);
-      });
-    })
-  }
+    }
 
-  createCategories(ctgs: any): Promise<Array<any>> {
-    let results: Array<any> = [];
-    return new Promise(async (resolve, reject) => {
-      let categories: Array<string> = _.map(ctgs, ctg => _.trim(ctg));
-      categories = _.filter(categories, ctg => !_.isEmpty(ctg));
-      if (_.isEmpty(categories)) resolve([]);
-
-      for (let ctg of categories) {
-        let categorie: any = await this.createProductCategory(ctg);
-        if (!categorie) continue;
-        results.push(categorie);
-      }
-
-      resolve(results);
-
-    });
-  }
-
-  private createProductCategory(category: string): Promise<any> {
-    return new Promise((resolve) => {
-      if (_.isEmpty(category)) resolve(false);
-      let args: any = {
-        name: category
-      };
-      this.Woocommerce.post('products/categories', args, (err, data, res) => {
-        resolve(JSON.parse(res));
-      });
-    })
-  }
+    private createProductCategory(category: string): Promise<any> {
+        return new Promise((resolve) => {
+            if (_.isEmpty(category)) resolve(false);
+            let args: any = {
+                name: category
+            };
+            this.Woocommerce.post('products/categories', args, (err, data, res) => {
+                let response: any = JSON.parse(res);
+                if (_.isUndefined(response.code)) {
+                    resolve(response);
+                } else {
+                    if (response.code === 'term_exists'){
+                        resolve({id: response.data.resource_id});
+                    }
+                    resolve(false);
+                }
+            });
+        })
+    }
 
 
-  errorFn(err: any, file: any) {
-    console.warn("ERROR:", err, file);
-    Helpers.setLoading(false);
-  }
+    errorFn(err: any, file: any) {
+        console.warn("ERROR:", err, file);
+        Helpers.setLoading(false);
+    }
 
 }
--- src/app/pages/supplier/edit-article/edit-article.component.ts
+++ src/app/pages/supplier/edit-article/edit-article.component.ts
@@ -13,164 +13,174 @@ import * as moment from 'moment'
 declare var $: any;
 
 @Component({
-  selector: 'app-edit-article',
-  templateUrl: './edit-article.component.html',
-  styleUrls: ['./edit-article.component.css']
+    selector: 'app-edit-article',
+    templateUrl: './edit-article.component.html',
+    styleUrls: ['./edit-article.component.css']
 })
 export class EditArticleComponent implements OnInit, OnChanges {
-  public ID: number = 0;
-  private WP: any;
-  public Form: FormGroup;
-  public Products: Array<any> = [];
-  public Suppliers: Array<any> = [];
-  public postResponseCache = new Map();
-  public notice: any = null;
-  @Input() Article: any;
-  @Output() refresh = new EventEmitter<any>();
-  constructor(
-    private http: HttpClient,
-    private apiWP: ApiWordpressService,
-    private cd: ChangeDetectorRef
-  ) {
-    this.Form = new FormGroup({
-      title: new FormControl('', Validators.required),
-      price: new FormControl(0, Validators.required),
-      product_id: new FormControl({value: null, disabled: true}, Validators.required),
-      user_id: new FormControl(null, Validators.required),
-      stock: new FormControl(null, Validators.required)
-    });
-    this.WP = this.apiWP.getWPAPI();
-  }
-
-  ngOnInit() {
-    $('#edit-article-supplier-modal').on('hide.bs.modal', e => {
-      Helpers.setLoading(false);
-    });
-  }
-
-  onSelectProduct($event) {
-    this.Form.patchValue({ title: $event.title.rendered });
-  }
-
-  loadPost(type: string): Observable<any> {
-    const URL = `https://${environment.SITE_URL}/wp-json/wp/v2/${type}?per_page=100`;
-    const postCache = this.postResponseCache.get(URL);
-    if (postCache) {
-      return of(postCache);
-    }
-    const response = this.http.get<any>(URL);
-    response.subscribe(posts => this.postResponseCache.set(URL, posts));
-
-    return response
-  }
-
-  loadSuppliers(): Observable<any> {
-    const URL = `https://${environment.SITE_URL}/wp-json/wp/v2/users?roles=fz-supplier`;
-    const postCache = this.postResponseCache.get(URL);
-    if (postCache) {
-      return of(postCache);
-    }
-    const response = this.http.get<any>(URL);
-    response.subscribe(suppliers => this.postResponseCache.set(URL, suppliers));
-
-    return response
-  }
-
-
-  get f() { return this.Form.controls; }
-
-  ngOnChanges(changes: SimpleChanges): void | boolean {
-    if ( ! _.isUndefined(changes.Article.currentValue) && ! _.isEmpty(changes.Article.currentValue)) {
-      let cV: any = changes.Article.currentValue;
-      this.ID = _.isUndefined(cV.id) ? (_.isUndefined(cV.ID) ? 0 : cV.ID) : cV.id;
-      if (this.ID === 0) return false;
-      this.initValues();
-      return true;
-    }
-  }
-
-  add_notice(msg: string, classes?: string) {
-    this.notice = {};
-    this.notice.msg = msg;
-    this.notice.classes = _.isUndefined(classes) ? 'warning' : classes;
-  }
-
-  /**
-   * Envoyer la formulaire de modification pour un article
-   */
-  onSubmit(): void | boolean {
-    if (this.Form.invalid) {
-      this.add_notice("Le formulaire est invalide", "danger");
-      return false;
-    }
-
-    if ( ! this.Form.dirty ) {
-      this.add_notice("Aucune modifications n'ont été apportées", 'warning');
-      return false;
-    }
-    const Values = this.Form.value;
-    Helpers.setLoading(true);
-    this.WP.fz_product().id(this.ID).update({
-      title: Values.title,
-      price: Values.price,
-      product_id: parseInt(Values.product_id),
-      user_id: parseInt(Values.user_id),
-      total_sales: parseInt(Values.stock),
-      date_review: moment().format('YYYY-MM-DD HH:mm:ss')
-    }).then(article => {
-      $('#edit-article-supplier-modal').modal('hide');
-      this.refresh.emit();
-    })
-  }
-
-  /**
-   * Initialiser et afficher la boite de dialogue pour la modification
-   */
-  initValues() {
-    Helpers.setLoading(true);
-    this.notice = null;
-    const ObsProduct = this.loadPost('product');
-    const ObsSuppliers = this.loadSuppliers();
-    const Zip = Observable.zip(ObsProduct, ObsSuppliers);
-    Zip.subscribe(results => {
-      this.Products = _.isEmpty(this.Products) ? results[0] : this.Products;
-      this.Suppliers = _.isEmpty(this.Suppliers) ? results[1] : this.Suppliers;
-      this.Form.patchValue({
-        title: this.Article.title.rendered,
-        price: this.Article.price,
-        product_id: this.Article.product_id,
-        user_id: this.Article.user_id,
-        stock: this.Article.total_sales
-      });
-      this.cd.detectChanges();
-      $('#edit-article-supplier-modal').modal('show');
-    }, error => {}, () => {
-      Helpers.setLoading(false);
-    } );
-  }
-
-  /**
-   * Supprimer une article dans la base de donnée
-   * @param id 
-   */
-  onRemoveArticle(id: number): void | boolean {
-    if (!_.isNumber(id)) return false;
-    $('#edit-article-supplier-modal').modal('hide');
-    Swal.fire({
-      title: "Confirmation",
-      text: "Voulez vous vraiment supprimer cette article?",
-      type: 'warning',
-      showCancelButton: true
-    }).then(result => {
-      if (result.value) {
-        Helpers.setLoading(true);
-        this.WP.fz_product().id(id).delete({force: true, reassign: 1}).then(resp => {
-          this.refresh.emit();
-          Helpers.setLoading(false);
-          Swal.fire("Succès", "Article supprimer avec succès", 'success');
+    public ID: number = 0;
+    private WP: any;
+    public Form: FormGroup;
+    public Products: Array<any> = [];
+    public Suppliers: Array<any> = [];
+    public postResponseCache = new Map();
+    public notice: any = null;
+    public dateReview: string;
+    public dateReviewFromNow: string;
+    @Input() Article: any;
+    @Output() refresh = new EventEmitter<any>();
+    constructor(
+        private http: HttpClient,
+        private apiWP: ApiWordpressService,
+        private cd: ChangeDetectorRef
+    ) {
+        this.Form = new FormGroup({
+            title: new FormControl('', Validators.required),
+            price: new FormControl(0, Validators.required),
+            product_id: new FormControl({ value: null, disabled: true }, Validators.required),
+            user_id: new FormControl(null, Validators.required),
+            stock: new FormControl(null, Validators.required)
         });
-      }
-    })
-  }
+        this.WP = this.apiWP.getWPAPI();
+    }
+
+    ngOnInit() {
+        moment.locale('fr');
+        $('#edit-article-supplier-modal').on('hide.bs.modal', e => {
+            Helpers.setLoading(false);
+        });
+    }
+
+    onSelectProduct($event) {
+        this.Form.patchValue({ title: $event.title.rendered });
+    }
+
+    loadPost(type: string): Observable<any> {
+        const URL = `https://${environment.SITE_URL}/wp-json/wp/v2/${type}?per_page=100`;
+        const postCache = this.postResponseCache.get(URL);
+        if (postCache) {
+            return of(postCache);
+        }
+        const response = this.http.get<any>(URL);
+        response.subscribe(posts => this.postResponseCache.set(URL, posts));
+
+        return response
+    }
+
+    loadSuppliers(): Observable<any> {
+        const URL = `https://${environment.SITE_URL}/wp-json/wp/v2/users?roles=fz-supplier`;
+        const postCache = this.postResponseCache.get(URL);
+        if (postCache) {
+            return of(postCache);
+        }
+        const response = this.http.get<any>(URL);
+        response.subscribe(suppliers => this.postResponseCache.set(URL, suppliers));
+
+        return response
+    }
+
+
+    get f() { return this.Form.controls; }
+
+    ngOnChanges(changes: SimpleChanges): void | boolean {
+        if (!_.isUndefined(changes.Article.currentValue) && !_.isEmpty(changes.Article.currentValue)) {
+            let cV: any = changes.Article.currentValue;
+            this.ID = _.isUndefined(cV.id) ? (_.isUndefined(cV.ID) ? 0 : cV.ID) : cV.id;
+            if (this.ID === 0) return false;
+            this.initValues();
+            return true;
+        }
+    }
+
+    add_notice(msg: string, classes?: string) {
+        this.notice = {};
+        this.notice.msg = msg;
+        this.notice.classes = _.isUndefined(classes) ? 'warning' : classes;
+    }
+
+    /**
+     * Envoyer la formulaire de modification pour un article
+     */
+    onSubmit(): void | boolean {
+        if (this.Form.invalid) {
+            this.add_notice("Le formulaire est invalide", "danger");
+            return false;
+        }
+
+        if (!this.Form.dirty) {
+            this.add_notice("Aucune modifications n'ont été apportées", 'warning');
+            return false;
+        }
+        const Values = this.Form.value;
+        Helpers.setLoading(true);
+        this.WP.fz_product().id(this.ID).update({
+            title: Values.title,
+            price: Values.price,
+            product_id: parseInt(Values.product_id),
+            user_id: parseInt(Values.user_id),
+            total_sales: parseInt(Values.stock),
+            date_review: moment().format('YYYY-MM-DD HH:mm:ss')
+        }).then(() => {
+            $('#edit-article-supplier-modal').modal('hide');
+            this.refresh.emit();
+        })
+    }
+
+    /**
+     * Initialiser et afficher la boite de dialogue pour la modification
+     */
+    initValues() {
+        Helpers.setLoading(true);
+        this.notice = null;
+        const ObsProduct = this.loadPost('product');
+        const ObsSuppliers = this.loadSuppliers();
+        const Zip = Observable.zip(ObsProduct, ObsSuppliers);
+        Zip.subscribe(results => {
+            this.Products = _.isEmpty(this.Products) ? results[0] : this.Products;
+            this.Suppliers = _.isEmpty(this.Suppliers) ? results[1] : this.Suppliers;
+            this.Form.patchValue({
+                title: this.Article.title.rendered,
+                price: this.Article.price,
+                product_id: this.Article.product_id,
+                user_id: this.Article.user_id,
+                stock: this.Article.total_sales
+            });
+
+            this.Form.controls.user_id.disable();
+            this.Form.controls.product_id.disable();
+
+            this.dateReview = moment(this.Article.date_review).format('LLL');
+            this.dateReviewFromNow = moment(this.Article.date_review).fromNow();
+
+            this.cd.detectChanges();
+            $('#edit-article-supplier-modal').modal('show');
+        }, error => { }, () => {
+            Helpers.setLoading(false);
+        });
+    }
+
+    /**
+     * Supprimer une article dans la base de donnée
+     * @param id 
+     */
+    onRemoveArticle(id: number): void | boolean {
+        if (!_.isNumber(id)) return false;
+        $('#edit-article-supplier-modal').modal('hide');
+        Swal.fire({
+            title: "Confirmation",
+            text: "Voulez vous vraiment supprimer cette article?",
+            type: 'warning',
+            showCancelButton: true
+        }).then(result => {
+            if (result.value) {
+                Helpers.setLoading(true);
+                this.WP.fz_product().id(id).delete({ force: true, reassign: 1 }).then(resp => {
+                    this.refresh.emit();
+                    Helpers.setLoading(false);
+                    Swal.fire("Succès", "Article supprimer avec succès", 'success');
+                });
+            }
+        })
+    }
 
 }
