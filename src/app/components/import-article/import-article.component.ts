17667e7092504e56433924ab99078eef48a972a7
Tiafeno <tiafenofnel@gmail.com>
2019-06-24 01:15:00 +0300

    Modifier le composant importation

--- src/app/components/import-article/import-article.component.ts
+++ src/app/components/import-article/import-article.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
 import * as Papa from 'papaparse';
 import * as _ from 'lodash';
-import * as moment from 'moment';
 declare var $: any;
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ApiWordpressService } from '../../_services/api-wordpress.service';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/zip';
 import { Helpers } from '../../helpers';
-import { ApiWoocommerceService } from '../../_services/api-woocommerce.service';
+import {HttpClient} from '@angular/common/http';
+import {config} from "../../../environments/environment";
 
 @Component({
     selector: 'app-import-article',
@@ -47,18 +45,13 @@ export class ImportArticleComponent implements OnInit {
     ];
 
     public Inputs: Array<any>;
-    private Wordpress: any;
-    private Woocommerce: any;
 
     @Output() refresh = new EventEmitter();
     constructor(
-        private apiWP: ApiWordpressService,
-        private apiWC: ApiWoocommerceService,
+      private Http: HttpClient,
         private cd: ChangeDetectorRef
     ) {
         this.Form = new FormGroup({ csv: new FormControl({ value: '' }, Validators.required) });
-        this.Wordpress = this.apiWP.getWPAPI();
-        this.Woocommerce = this.apiWC.getWoocommerce();
     }
 
     ngOnInit() {
@@ -96,9 +89,10 @@ export class ImportArticleComponent implements OnInit {
         Papa.parse(this.Files, {
             delimiter: ';',
             preview: 0,
-            step: async (results: any, parser: any) => {
+            step: (results: any, parser: any) => {
                 parser.pause();
                 if (results || results.data) {
+                    const Form: FormData = new FormData();
                     const column: any = results.data;
                     const price: number = parseInt(column[this.Columns.Price], 10);
                     if (_.isNaN(price)) {
@@ -107,67 +101,21 @@ export class ImportArticleComponent implements OnInit {
                         return;
                     }
 
-                    const categorie: string = column[this.Columns.Categorie];
-                    let _categories: Array<any> = await this.createCategories(_.split(categorie, ','));
-                    _categories = _.map(_categories, (ctg) => { return {id: ctg.id}; });
-                    const args: object = {
-                        type: 'simple',
-                        status: 'publish',
-                        name: column[this.Columns.Title],
-                        regular_price: '0',
-                        description: column[this.Columns.Description],
-                        short_description: '',
-                        categories: _categories,
-                        attributes: [
-                            {
-                                name: 'brands',
-                                options: column[this.Columns.Mark],
-                                visible: true
-                            }
-                        ],
-                        meta_data: [
-                            { key: '_fz_marge', value: column[this.Columns.Marge] },
-                            { key: '_fz_marge_dealer', value: column[this.Columns.MargeDealer] }
-                        ],
-                        images: []
-                    };
-                    const productRef = this.createProduct(args);
-                    const supplier = this.Wordpress.users().roles('fz-supplier').filter(
-                      {
-                        meta_key: 'reference',
-                        meta_value: column[this.Columns.SupplierRef]
-                      });
-                    Observable.zip(productRef, supplier).subscribe(dataObs => {
-                        // return value with pagination (property: _paging)
-                        const productObs: any = _.isArray(dataObs) ? dataObs[0] : dataObs;
-                        const supplierObs: any = _.isArray(dataObs[1]) && !_.isUndefined(dataObs[1][0]) ? dataObs[1][0] : null;
-                        if ( ! _.isObject(productObs) || ! _.isObject(supplierObs)) {
-                            parser.resume();
-                            return;
-                        }
-
-                        const _price: string = column[this.Columns.Price];
-                        const _priceDealer: string = column[this.Columns.PriceDealer];
-
-                        this.Wordpress
-                            .fz_product()
-                            .create({
-                                title: _.isNull(this.Columns.Title) ? productObs.name : column[this.Columns.Title],
-                                content: productObs.description,
-                                status: 'publish',
-                                price: _price,
-                                price_dealer: _priceDealer,
-                                date_add: moment().format('YYYY-MM-DD HH:mm:ss'),
-                                date_review: moment().format('YYYY-MM-DD HH:mm:ss'),
-                                product_id: productObs.id,
-                                total_sales: this.Columns.Qty.toString(),
-                                user_id: supplierObs.id,
-                                product_cat: _.map(productObs.categories, (ctg) => ctg.id)
-                            }).then(() => {
-                                parser.resume();
-                            }).catch(function (err) {
-                                parser.abord();
-                            });
+                    Form.append('name', column[this.Columns.Title]);
+                    Form.append('regular_price', '0');
+                    Form.append('price', column[this.Columns.Price]);
+                    Form.append('price_dealer', column[this.Columns.PriceDealer]);
+                    Form.append('description', column[this.Columns.Description]);
+                    Form.append('short_description', '');
+                    Form.append('mark', column[this.Columns.Mark]);
+                    Form.append('marge', column[this.Columns.Marge]);
+                    Form.append('marge_dealer', column[this.Columns.MargeDealer]);
+                    Form.append('reference', column[this.Columns.SupplierRef]);
+                    Form.append('quantity', '1');
+                    Form.append('categories', column[this.Columns.Categorie]);
+                    this.Http.post<any>(`${config.apiUrl}/import/csv/articles`, Form, ).subscribe(resp => {
+                      console.log(resp);
+                      parser.resume();
                     });
                 } else {
                     parser.resume();
@@ -188,61 +136,6 @@ export class ImportArticleComponent implements OnInit {
             }
         });
     }
-
-    createProduct(args: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.Woocommerce.post('products', args, (err, data, response) => {
-                const product: any = JSON.parse(response);
-                this.Woocommerce.put(`products/${product.id}`, { sku: `PRD${product.id}` }, (err, data, res) => {
-                    const response: any = JSON.parse(res);
-                    if (_.isUndefined(response.code)) {
-                        resolve(response);
-                    } else {
-                        resolve({ id: response.data.resource_id, name: '', description: '' });
-                    }
-                });
-            });
-        });
-    }
-
-    private createCategories(ctgs: any): Promise<Array<any>> {
-        const results: Array<any> = [];
-        return new Promise(async (resolve, reject) => {
-            let categories: Array<string> = _.map(ctgs, ctg => _.trim(ctg));
-            categories = _.filter(categories, ctg => !_.isEmpty(ctg));
-            if (_.isEmpty(categories)) resolve([]);
-
-            for (const ctg of categories) {
-                const categorie: any = await this.createProductCategory(ctg);
-                if (!categorie) continue;
-                results.push(categorie);
-            }
-
-            resolve(results);
-
-        });
-    }
-
-    private createProductCategory(category: string): Promise<any> {
-        return new Promise((resolve) => {
-            if (_.isEmpty(category)) resolve(false);
-            const args: any = {
-                name: category
-            };
-            this.Woocommerce.post('products/categories', args, (err, data, res) => {
-                const response: any = JSON.parse(res);
-                if (_.isUndefined(response.code)) {
-                    resolve(response);
-                } else {
-                    if (response.code === 'term_exists') {
-                        resolve({ id: response.data.resource_id });
-                    }
-                    resolve(false);
-                }
-            });
-        });
-    }
-
 
     errorFn(err: any, file: any) {
         console.warn('ERROR:', err, file);
--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiWordpressService} from "../../_services/api-wordpress.service";
+import {config} from "../../../environments/environment";
+import {Helpers} from "../../helpers";
+import * as _ from 'lodash';
+import * as moment from 'moment';
+declare var $: any;
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-clients',
@@ -7,9 +14,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
-  constructor() { }
+  private WPAPI: any;
+  public Table: any;
 
-  ngOnInit() {
+  constructor(
+    private apiWp: ApiWordpressService,
+    private router: Router
+  ) {
+    this.WPAPI = this.apiWp.getWPAPI();
   }
 
+  public reload() {
+    this.Table.ajax.reload(null, false);
+  }
+
+  ngOnInit() {
+    moment.locale('fr');
+
+    const getElementData = (ev: any): any => {
+      const el: any = $(ev.currentTarget).parents('tr');
+      const data: any = this.Table.row(el).data();
+      return data;
+    };
+
+    const productsTable = $('#clients-table');
+    this.Table = productsTable.DataTable({
+      pageLength: 10,
+      page: 1,
+      ordering: false, // Activer ou désactiver l'affichage d'ordre
+      fixedHeader: true,
+      responsive: false,
+      sDom: 'rtip',
+      processing: true,
+      serverSide: true,
+      columns: [
+        {
+          data: 'id', render: (data) => {
+            return `<span>${data}</span>`;
+          }
+        },
+        {
+          data: 'first_name', render: (data, type, row) => {
+            return `<span class="edit-product font-strong">${data} ${row.last_name}</span>`;
+          }
+        },
+        {
+          data: 'email', render: (data, type, row) => {
+            return `<a href="mailto:${data}" target="_blank">${data}</span>`;
+          }
+        },
+        {
+          data: 'role_office', render: (data, type, row) => {
+            const status: string = _.isEmpty(data) || _.isNull(data) ? 'En attente': (data === 1 ? 'Acheteur' : 'Revendeur');
+            return `<span class="badge badge-pink">${status}</span>`;
+          }
+        },
+        {
+          data: 'reference', render: (data) => {
+            return _.isEmpty(data) ? 'Non renseigner' : data;
+          }
+        },
+        {
+          data: 'registered_date', render: (data) => {
+            return moment(data).fromNow();
+          }
+        },
+        {
+          data: null,
+          render: (data, type, row, meta) => {
+            return `
+                  <div class="fab fab-left">
+                     <button class="btn btn-sm btn-primary btn-icon-only btn-circle btn-air" data-toggle="button">
+                        <i class="fab-icon la la-bars"></i>
+                        <i class="fab-icon-active la la-close"></i>
+                     </button>
+                     <ul class="fab-menu">
+                        <li><button class="btn btn-primary btn-icon-only btn-circle btn-air edit-product"><i class="la la-edit"></i></button></li>
+                     </ul>
+                  </div>`;
+          }
+        }
+      ],
+      initComplete: (setting, json) => {
+        $('#clients-table tbody').on('click', '.edit-client', e => {
+          e.preventDefault();
+          const __clt: any = getElementData(e);
+        });
+
+      },
+      ajax: {
+        url: `${config.apiUrl}/clients/`,
+        dataType: 'json',
+        data: {
+          order: false,
+        },
+        beforeSend: function (xhr) {
+          const __fzCurrentUser: any = JSON.parse(localStorage.getItem('__fzCurrentUser'));
+          if (__fzCurrentUser && __fzCurrentUser.token) {
+            xhr.setRequestHeader('Authorization',
+              `Bearer ${__fzCurrentUser.token}`);
+          }
+        },
+        type: 'POST',
+      }
+    });
+  }
 }
