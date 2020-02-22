04ceb3bc169cc83c74f84f590894fb77587aeaa1
Tiafeno <tiafenofnel@gmail.com>
2020-02-22 18:22:38 +0300

    Featured

---
+++ src/app/annonces/annonce-edit/annonce-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-annonce-edit',
+  templateUrl: './annonce-edit.component.html',
+  styleUrls: ['./annonce-edit.component.css']
+})
+export class AnnonceEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
